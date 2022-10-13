1a8309c0a82a492b4f91abc76a4d959b02dfc4e4
Alexis <alexislopez@outlook.com.ar>
2022-10-13 17:14:31 -0300

    emprolijo codigo y agrego docstring

--- Clases/clases.py
+++ Clases/clases.py
@@ -2,6 +2,13 @@ from math import pi
 
 
 class Circulo:
+
+    """Clase que instancia un objeto con las propiedades de un circulo.\n
+    Recibe un valor tipo float que representa el radio.\n
+    Contiene metodos para calcular el perimetro y el area de acorde al radio dado.\n
+    Utiliza los metodos __mul__ para multiplicar el radio con otro numero, __str__ para imprimir las caracteristicas del circulo, y set_radius para modificar el valor del radio."""
+
+
     def __init__(self, r):
         self.radius = r
 
@@ -16,9 +23,9 @@ class Circulo:
 
 
     def per(self):
-        perimetro = float(self.radius) * pi * 2
-        perimetro = round(perimetro, 2)
-        return 'El perimetro del circulo es de '+str(perimetro)+'cm'
+        perimeter = float(self.radius) * pi * 2
+        perimeter = round(perimeter, 2)
+        return 'El perimetro del circulo es de '+str(perimeter)+'cm'
         
 
     def ar(self):
@@ -30,14 +37,23 @@ class Circulo:
         self.radius = num
 
 
-def set_radio():
-    r = float(input('Ingrese el valor del radio del circulo en cm: '))
-    if r <= 0:
+def set_radius():
+
+    """Funcion destinada a permitir que el usuario asigne un valor numerico a ser usado como radio del circulo.\n
+    Si el valor no es numerico o no es positivo eleva un error"""
+
+    r = input('Ingrese el valor del radio del circulo en cm: ')
+    if not r.isnumeric() or float(r) <= 0:
         raise Exception('Lamentablemente no se puede tener circulos con radios no positivos, por favor vuelva a intentarlo con un numero positivo')
-    return r
+    return float(r)
 
 
 def options():
+
+    """Funcion que utiliza input para que el usuario elija entre dos opciones.\n
+    Retorna el valor elegido.\n
+    Si se ingresa un valor que no esta dentro de las opciones eleva un error tipo Exception"""
+
     res = input("""Elija que desea hacer (ingrese el numero de la opcion a realizar): \n 
             [1] Multiplicar el radio por un factor \n
             [2] Modificar el radio \n
@@ -48,23 +64,24 @@ def options():
 
 
 def run():
-    radio = set_radio()
-    circulo = Circulo(radio)
-    print(circulo)
+    radius = set_radius()
+    circle = Circulo(radius)
+    print(circle)
     ans = options()
     while ans == '1':
         factor = float(input('Por favor ingrese el numero por el cual desea multiplicar el radio: '))
-        mutlipied_circulo = circulo * factor
-        print(mutlipied_circulo)
+        mutlipied_circle = circle * factor
+        print(mutlipied_circle)
         ans = options()
     while ans == '2':
-        radio2 = set_radio()
-        circulo.set_radius(radio2)
+        new_radius = set_radius()
+        circle.set_radius(new_radius)
+        print(circle)
         ans = options()
         while ans == '1':
             factor = float(input('Por favor ingrese el numero por el cual desea multiplicar el radio: '))
-            mutlipied_circulo = circulo * factor
-            print(mutlipied_circulo)
+            mutlipied_circle = circle * factor
+            print(mutlipied_circle)
             ans = options()
         
 
